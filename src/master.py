8e99b8622f20c8e92292fd63907ef4429270cd2b
stevensotelo <stevenbetancurt@hotmail.com>
2022-02-24 16:43:30 -0500

    worked in treaty. added merge countries, income, germplasm and fao

--- src/master.py
+++ src/master.py
@@ -36,14 +36,46 @@ treaty_encoding = conf_general.loc[conf_general["variable"] == "treaty_encoding"
 treaty_file = conf_general.loc[conf_general["variable"] == "treaty_file","value"].values[0]
 treaty_sheet = conf_general.loc[conf_general["variable"] == "treaty_sheet","value"].values[0]
 treaty_key_crop = conf_general.loc[conf_general["variable"] == "treaty_key_crop","value"].values[0]
+treaty_key_country_origin = conf_general.loc[conf_general["variable"] == "treaty_key_country_origin","value"].values[0]
+treaty_key_country_recipient = conf_general.loc[conf_general["variable"] == "treaty_key_country_recipient","value"].values[0]
+treaty_year = conf_general.loc[conf_general["variable"] == "treaty_year","value"].values[0]
+treaty_key_germplasm = conf_general.loc[conf_general["variable"] == "treaty_key_germplasm","value"].values[0]
 plant_treaty_fields = str(conf_general.loc[conf_general["variable"] == "plant_treaty_fields","value"].values[0]).split(',')
 plant_treaty_key_crop = conf_general.loc[conf_general["variable"] == "plant_treaty_key_crop","value"].values[0]
-
+countries_file = conf_general.loc[conf_general["variable"] == "countries_file","value"].values[0]
+countries_sheet = conf_general.loc[conf_general["variable"] == "countries_sheet","value"].values[0]
+countries_fields = str(conf_general.loc[conf_general["variable"] == "countries_fields","value"].values[0]).split(',')
+countries_key_country = conf_general.loc[conf_general["variable"] == "countries_key_country","value"].values[0]
+income_file = conf_general.loc[conf_general["variable"] == "income_file","value"].values[0]
+income_sheet = conf_general.loc[conf_general["variable"] == "income_sheet","value"].values[0]
+income_key = conf_general.loc[conf_general["variable"] == "income_key","value"].values[0]
+income_years = [int(y) for y in str(conf_general.loc[conf_general["variable"] == "income_years","value"].values[0]).split(',')]
+germplasm_file = conf_general.loc[conf_general["variable"] == "germplasm_file","value"].values[0]
+germplasm_sheet = conf_general.loc[conf_general["variable"] == "germplasm_sheet","value"].values[0]
+germplasm_key = conf_general.loc[conf_general["variable"] == "germplasm_key","value"].values[0]
+fao_file = conf_general.loc[conf_general["variable"] == "fao_file","value"].values[0]
+fao_years = str(conf_general.loc[conf_general["variable"] == "fao_years","value"].values[0]).split(',')
+fao_elements_col = conf_general.loc[conf_general["variable"] == "fao_elements_col","value"].values[0]
+fao_key_crop = conf_general.loc[conf_general["variable"] == "fao_key_crop","value"].values[0]
+fao_encoding = conf_general.loc[conf_general["variable"] == "fao_encoding","value"].values[0]
 ##############################################
 # 02 - Processing Treaty Data
 ##############################################
 
 print("02 - Processing Treaty Data")
 treaty = Treaty(inputs_folder, treaty_file, treaty_sheet, workspace_folder)
-treaty.merge_uses(conf_xls.parse("plant_treaty"), plant_treaty_fields, plant_treaty_key_crop, treaty_key_crop, force=True)
+print("Merging with plant treaty data")
+treaty.merge_plant_treaty(conf_xls.parse("plant_treaty"), plant_treaty_fields, plant_treaty_key_crop, treaty_key_crop)
+print("Merging with countries data")
+treaty.merge_countries(treaty_key_country_origin,treaty_key_country_recipient,countries_file,countries_sheet, countries_fields,countries_key_country)
+print("Merging with income data")
+treaty.merge_income(treaty_key_country_origin,treaty_key_country_recipient, treaty_year,income_file,income_sheet,income_key,income_years)
+print("Merging with germplasm data")
+treaty.merge_germplasm(treaty_key_germplasm,germplasm_file, germplasm_sheet,germplasm_key)
+print("Merging with fato data")
+treaty.merge_fao(treaty_key_crop,fao_file,fao_years,fao_elements_col,fao_key_crop,fao_encoding)
+
+print("Fixing columns names")
+treaty.change_names(conf_xls.parse("treaty_columns"))
+
 
--- src/treaty.py
+++ src/treaty.py
@@ -24,11 +24,14 @@ class Treaty(object):
     def getinputfile(self):
         return os.path.join(self.inputs_folder,self.input_file)
 
+    def getworkspacestep(self,step,folder="OK"):
+        return os.path.join(self.getworkspace(),step,folder)
+
     ## Method construct
     # (string) input: Name of the raw data
     # (string) workspace: Place where data processed should saved
     # (string) encoding: Encoding files. By default it is ISO-8859-1
-    def __init__(self, inputs_folder, input_file, input_sheet, workspace, outputs= 'treaty', encoding="ISO-8859-1"):
+    def __init__(self, inputs_folder, input_file, input_sheet, workspace, outputs= 'treaty', encoding="utf-8"):
         self.inputs_folder = inputs_folder
         self.input_file = input_file
         self.input_sheet = input_sheet
@@ -36,6 +39,19 @@ class Treaty(object):
         self.encoding = encoding
         self.outputs = outputs
         mf.mkdir(self.getworkspace())
+
+    # Method with replace values for similar columns for records which has NA's data.
+    # When merge two dataframe with similar columns, pandas set the name x for left dataframe
+    # and y for right dataframe. It join both columns in just one
+    # (dataframe) df: Dataframe which has the columns that will be update
+    # (string) col: Column name
+    def update_columns_data(self, df,col):
+        x = col + "_x"
+        y = col + "_y"
+        df.loc[df[x].isna(),x] = df.loc[df[x].isna(),:][y]
+        df[col] = df[x]
+        df = df.drop([x, y], axis=1)
+        return df
 
     ## Method that merge the raw data with plant treaty.
     # It adds new fields which are needed for the analysis
@@ -46,7 +62,7 @@ class Treaty(object):
     # (string) step: prefix of the output files. By default it is 01
     # (bool) force: Set if the process have to for the execution of all files even if the were processed before.
     #               By default it is False
-    def merge_uses(self, plant_treaty, plant_treaty_fields, plant_treaty_key, crop_key, step="01",force=False):
+    def merge_plant_treaty(self, plant_treaty, plant_treaty_fields, plant_treaty_key, crop_key, step="01",force=False):
         final_path = os.path.join(self.getworkspace(),step)
         mf.create_review_folders(final_path,sm=False)
         # It checks if files should be force to process again or if the path exist
@@ -59,10 +75,12 @@ class Treaty(object):
             fields_list.append(plant_treaty_key)
             df_merged = pd.merge(df,plant_treaty[fields_list],how='left',left_on=[crop_key],right_on=[plant_treaty_key])
             print("\tNumber rows: treaty=",df.shape[0],"plant treaty=",plant_treaty.shape[0],"merged=",df_merged.shape[0])
+
             # Updating the Uses for treaty data
-            df_merged.loc[df_merged["Use - detailed_x"].isna(),"Use - detailed_x"] = df_merged.loc[df_merged["Use - detailed_x"].isna(),:]["Use - detailed_y"]
-            df_merged["Use - detailed"] = df_merged["Use - detailed_x"]
-            df_merged = df_merged.drop(['Use - detailed_x', 'Use - detailed_y'], axis=1)
+            print("\tMergin columns with similar data")
+            df_merged = self.update_columns_data(df_merged,"Use - detailed")
+            df_merged = self.update_columns_data(df_merged,"Use - primary")
+            df_merged = self.update_columns_data(df_merged,"Included in MLS Annex 1")
 
             # Saving
             print("\tSaving ", final_file)
@@ -73,30 +91,187 @@ class Treaty(object):
 
     ## Method that merge the raw data with countries.
     # It allows to add new information like region and subregions
+    # (string) origin: Field name for key of country from origin
+    # (string) recipient: Field name for key of country from recipient
+    # (string) countries_file: Files name of countries. It should be into inputs folder
+    # (string) countries_sheet: Sheet name
+    # (string) countries_fields: List of field which will be used
+    # (string) countries_key: Key field for merging data with countries
     # (string) step: prefix of the output files. By default it is 02
     # (bool) force: Set if the process have to for the execution of all files even if the were processed before.
     #               By default it is False
-    def merge_countries(self, step="02",force=False):
+    def merge_countries(self, origin, recipient, countries_file, countries_sheet, countries_fields, countries_key, step="02",force=False):
         final_path = os.path.join(self.getworkspace(),step)
-        mf.create_review_folders(final_path,sm=False)
+        mf.create_review_folders(final_path)
         # It checks if files should be force to process again or if the path exist
         final_file = os.path.join(final_path,"OK",self.outputs + ".csv")
         if force or not os.path.exists(final_file):
-            """ print("\tProcessing ", self.getinputfile())
-            data_xls = pd.ExcelFile(self.getinputfile())
-            df = data_xls.parse(self.input_sheet)
-            fields_list = plant_treaty_fields
-            fields_list.append(plant_treaty_key)
-            df_merged = pd.merge(df,plant_treaty[fields_list],how='left',left_on=[crop_key],right_on=[plant_treaty_key])
-            print("\tNumber rows: treaty=",df.shape[0],"plant treaty=",plant_treaty.shape[0],"merged=",df_merged.shape[0])
-            # Updating the Uses for treaty data
-            df_merged.loc[df_merged["Use - detailed_x"].isna(),"Use - detailed_x"] = df_merged.loc[df_merged["Use - detailed_x"].isna(),:]["Use - detailed_y"]
-            df_merged["Use - detailed"] = df_merged["Use - detailed_x"]
-            df_merged = df_merged.drop(['Use - detailed_x', 'Use - detailed_y'], axis=1)
+            countries_path = os.path.join(self.inputs_folder,countries_file)
+            print("\tProcessing countries=", countries_path,"data=",os.path.join(self.getworkspacestep("01"),self.outputs + ".csv"))
+            countries_xls = pd.ExcelFile(countries_path)
+            df_countries = countries_xls.parse(countries_sheet)
+            c_fields_list = countries_fields
+            #c_fields_list.append(countries_key)
+            df_countries = df_countries[c_fields_list]
+            df = pd.read_csv(os.path.join(self.getworkspacestep("01"),self.outputs + ".csv"), encoding = self.encoding)
+
+            print("\tMerging origin")
+            df_merged = pd.merge(df,df_countries,how="left",left_on=[origin],right_on=[countries_key])
+            print("\tNumber rows: treaty=",df.shape[0],"countries=",df_countries.shape[0],"merged=",df_merged.shape[0])
+
+            print("\tMerging recipient")
+            df_merged = pd.merge(df_merged,df_countries,how="left",left_on=[recipient],right_on=[countries_key])
+            print("\tNumber rows: treaty=",df.shape[0],"countries=",df_countries.shape[0],"merged=",df_merged.shape[0])
 
             # Saving
             print("\tSaving ", final_file)
-            df_merged.to_csv(final_file, index = False, encoding = self.encoding) """
+            df_merged.to_csv(final_file, index = False, encoding = self.encoding)
+
+        else:
+            print("\tNot processed: ",final_file)
+
+
+    ## Method that merge the data with income for each country.
+    # It add income for origin and recipient
+    # (string) origin: Field name for key of country from origin
+    # (string) recipient: Field name for key of country from recipient
+    # (string) treaty_year: Field name for year of transaction
+    # (string) income_file: File name of income for countries. It should be into inputs folder
+    # (string) income_sheet: Sheet name
+    # (string) income_key: Key field for merging data from income
+    # (int[]) income_years: Arrays of years
+    # (string) step: prefix of the output files. By default it is 03
+    # (bool) force: Set if the process have to for the execution of all files even if the were processed before.
+    #               By default it is False
+    def merge_income(self, origin, recipient, treaty_year, income_file, income_sheet, income_key, income_years, step="03",force=False):
+        final_path = os.path.join(self.getworkspace(),step)
+        mf.create_review_folders(final_path, sm=False)
+        # It checks if files should be force to process again or if the path exist
+        final_file = os.path.join(final_path,"OK",self.outputs + ".csv")
+        if force or not os.path.exists(final_file):
+            income_path = os.path.join(self.inputs_folder,income_file)
+            print("\tProcessing income=", income_path,"data=",os.path.join(self.getworkspacestep("02"),self.outputs + ".csv"))
+
+            income_xls = pd.ExcelFile(income_path)
+            df_income = income_xls.parse(income_sheet)
+
+            # Pivoting columns into rows
+            df_income = pd.melt(df_income, id_vars=[income_key], value_vars=income_years)
+
+            df = pd.read_csv(os.path.join(self.getworkspacestep("02"),self.outputs + ".csv"), encoding = self.encoding)
+
+            print("\tMerging origin")
+            df_merged = pd.merge(df,df_income,how="left",left_on=[origin,treaty_year],right_on=[income_key,"variable"])
+            print("\tNumber rows: treaty=",df.shape[0],"income=",df_income.shape[0],"merged=",df_merged.shape[0])
+
+            print("\tMerging recipient")
+            df_merged = pd.merge(df_merged,df_income,how="left",left_on=[recipient,treaty_year],right_on=[income_key,"variable"])
+            print("\tNumber rows: treaty=",df.shape[0],"income=",df_income.shape[0],"merged=",df_merged.shape[0])
+
+            df_merged = df_merged.drop(["variable_x","variable_y",income_key+"_x",income_key+"_y"], axis=1)
+
+            # Saving
+            print("\tSaving ", final_file)
+            df_merged.to_csv(final_file, index = False, encoding = self.encoding)
+
+        else:
+            print("\tNot processed: ",final_file)
+
+    ## Method that merge the data with germplasm for each crop.
+    # (string) treaty_key_germplasm: Field name for key of country from origin
+    # (string) germplasm_file: File name of income for countries. It should be into inputs folder
+    # (string) germplasm_sheet: Sheet name
+    # (string) germplasm_key: Key field for merging data from income
+    # (string) step: prefix of the output files. By default it is 03
+    # (bool) force: Set if the process have to for the execution of all files even if the were processed before.
+    #               By default it is False
+    def merge_germplasm(self, treaty_key_germplasm, germplasm_file, germplasm_sheet, germplasm_key, step="04",force=False):
+        final_path = os.path.join(self.getworkspace(),step)
+        mf.create_review_folders(final_path, sm=False)
+        # It checks if files should be force to process again or if the path exist
+        final_file = os.path.join(final_path,"OK",self.outputs + ".csv")
+        if force or not os.path.exists(final_file):
+            germ_path = os.path.join(self.inputs_folder,germplasm_file)
+            print("\tProcessing germplasm=", germ_path,"data=",os.path.join(self.getworkspacestep("03"),self.outputs + ".csv"))
+            germ_xls = pd.ExcelFile(germ_path)
+            df_germ = germ_xls.parse(germplasm_sheet)
+            df = pd.read_csv(os.path.join(self.getworkspacestep("03"),self.outputs + ".csv"), encoding = self.encoding)
+
+            print("\tMerging by crops")
+            df_merged = pd.merge(df,df_germ,how="left",left_on=[treaty_key_germplasm],right_on=[germplasm_key])
+            print("\tNumber rows: treaty=",df.shape[0],"income=",df_germ.shape[0],"merged=",df_merged.shape[0])
+
+            # Removing key id
+            df_merged = df_merged.drop([germplasm_key], axis=1)
+
+            # Saving
+            print("\tSaving ", final_file)
+            df_merged.to_csv(final_file, index = False, encoding = self.encoding)
+
+        else:
+            print("\tNot processed: ",final_file)
+
+    ## Method that merge the data with fao for each crop.
+    # (string) treaty_key_crop: Field name for key of crop
+    # (string) fao_file: File name of fao data. It should be into inputs folder
+    # (string) fao_years: List of year to calculate average
+    # (string) fao_elements_col: Column name which has elements or variables
+    # (string) fao_key_crop: Column names for key crop in fao
+    # (string) fao_encoding: Encoding of fao file
+    # (string) step: prefix of the output files. By default it is 03
+    # (bool) force: Set if the process have to for the execution of all files even if the were processed before.
+    #               By default it is False
+    def merge_fao(self, treaty_key_crop, fao_file, fao_years, fao_elements_col,fao_key_crop, fao_encoding, step="05",force=False):
+        final_path = os.path.join(self.getworkspace(),step)
+        mf.create_review_folders(final_path, sm=False)
+        # It checks if files should be force to process again or if the path exist
+        final_file = os.path.join(final_path,"OK",self.outputs + ".csv")
+        if force or not os.path.exists(final_file):
+            fao_path = os.path.join(self.inputs_folder,fao_file)
+            print("\tProcessing germplasm=", fao_path,"data=",os.path.join(self.getworkspacestep("04"),self.outputs + ".csv"))
+            df_fao = pd.read_csv(fao_path,encoding=fao_encoding) 
+            df = pd.read_csv(os.path.join(self.getworkspacestep("04"),self.outputs + ".csv"), encoding = self.encoding)
+
+            #df_fao = df_fao.groupby([fao_key_crop,fao_elements_col],as_index=False)[fao_years].mean()
+            df_fao["value"] = df_fao[fao_years].mean(axis=1)
+            #df_fao = pd.melt(df_fao, id_vars=[fao_key_crop,fao_elements_col], value_vars="value")
+            df_fao = df_fao.pivot_table(index=[fao_key_crop], columns=[fao_elements_col], values="value")
+            df_fao.reset_index(level=[fao_key_crop], inplace=True)
+
+            print("\tMerging by crops")
+            df_merged = pd.merge(df,df_fao,how="left",left_on=[treaty_key_crop],right_on=[fao_key_crop])
+            print("\tNumber rows: treaty=",df.shape[0],"income=",df_fao.shape[0],"merged=",df_merged.shape[0])
+
+
+            # Saving
+            print("\tSaving ", final_file)
+            df_merged.to_csv(final_file, index = False, encoding = self.encoding)
+            
+        else:
+            print("\tNot processed: ",final_file)
+
+    ## Method that change the columns names of dataset
+    # (string) origin: Field name for key of country from origin
+    # (string) recipient: Field name for key of country from recipient
+    # (string) countries_file: Files name of countries. It should be into inputs folder
+    # (string) countries_sheet: Sheet name
+    # (string) countries_fields: List of field which will be used
+    # (string) countries_key: Key field for merging data with countries
+    # (string) step: prefix of the output files. By default it is 02
+    # (bool) force: Set if the process have to for the execution of all files even if the were processed before.
+    #               By default it is False
+    def change_names(self, columns, step="06",force=False):
+        final_path = os.path.join(self.getworkspace(),step)
+        mf.create_review_folders(final_path, er=False,sm=False)
+        # It checks if files should be force to process again or if the path exist
+        final_file = os.path.join(final_path,"OK",self.outputs + ".csv")
+        if force or not os.path.exists(final_file):
+            print("\tProcessing data=",os.path.join(self.getworkspacestep("05"),self.outputs + ".csv"))
+            df = pd.read_csv(os.path.join(self.getworkspacestep("05"),self.outputs + ".csv"), encoding = self.encoding)
+            df.columns = columns["new"]
+            # Saving
+            print("\tSaving ", final_file)
+            df.to_csv(final_file, index = False, encoding = self.encoding)
 
         else:
             print("\tNot processed: ",final_file)
